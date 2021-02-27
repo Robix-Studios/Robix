7969993483b700494dcdcba99f17d93f872a7eb5
Miqek <79752101+Miqek@users.noreply.github.com>
2021-02-27 15:44:47 +0100

    Esborrany de pràctica

---
+++ Robix Unity/Assets/Codi/MovimentPlayer.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Events;
+
+public class MovimentPlayer : MonoBehaviour
+{
+    public float speed = 5;
+    private Rigidbody2D rigidBody;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidBody = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.D)) rigidBody.AddForce(new Vector2(3*speed, 0));
+        if (Input.GetKey(KeyCode.A)) rigidBody.AddForce(new Vector2(-3 * speed, 0));
+        if (Input.GetKey(KeyCode.Space)) rigidBody.AddForce(new Vector2(0, 5 * speed));
+    }
+}
